24fda797328cec426047e94dcb7830e9c360a02d
Giovanna-Cynthia <giovannacynthia34@gmail.com>
2024-08-06 13:55:55 -0300

    Tela de Cadastro

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+const telefone = document.querySelector("#telefone");
+const button = document.querySelector("button");
+const lista = document.querySelector(".lista");
+
+
+/* Eventos de JS */
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const inputNameValue = nome.value;
+    const inputEmailValue = email.value;
+    const inputTelefoneValue = telefone.value;
+    const templateHTML = `<li> 
+    Nome: ${inputNameValue} <br>
+    Email: ${inputEmailValue} <br> 
+    Telefone: ${inputTelefoneValue} <br>
+    <button class="excluir">Excluir</button>
+    </li>`;
+
+    /* Condição */ 
+    if (inputNameValue, inputEmailValue, inputTelefoneValue === "") {
+        alert("Preencha o campo vazio");
+        return false;
+    }
+
+    /*Incluindo itens no HTML*/
+    lista.innerHTML += templateHTML;
+
+    /* Limpando meu campo */
+    nome.value = "";
+    email.value = "";
+    telefone.value = "";
+
+    lista.addEventListener("click", (e) => {
+        if (e.target && e.target.classList.contains("excluir")) {
+            e.target.parentElement.remove();
+        }
+    });
+
+});
